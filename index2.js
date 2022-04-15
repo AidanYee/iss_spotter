8a25e1326c29e5f92714471441fcc18a61c394cd
Aidan Yee <aidan.yee@pm.me>
2022-04-15 03:11:14 +0000

    brought ISS Spotter to the PROMISE land

---
+++ index2.js
@@ -0,0 +1,19 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("ERROR: Check yo eyes. I think there is a bug in your code.", error.message);
+  });
